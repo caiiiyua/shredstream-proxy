3459264a92745faba8b8c8adcb38809d6c659f42
Cai Yuanqing <caiiiyua@gmail.com>
2025-06-05 15:48:39 +1200

    Testing deshred

--- examples/deshred.rs
+++ examples/deshred.rs
@@ -26,17 +26,19 @@ async fn main() -> Result<(), std::io::Error> {
                     continue;
                 }
             };
-        println!(
-            "slot {}, entries: {}, transactions: {}",
-            slot_entry.slot,
-            entries.len(),
-            entries.iter().map(|e| e.transactions.len()).sum::<usize>()
-        );
+        // println!(
+        //     "slot {}, entries: {}, transactions: {}",
+        //     slot_entry.slot,
+        //     entries.len(),
+        //     entries.iter().map(|e| e.transactions.len()).sum::<usize>()
+        // );
+        let entries_size = entries.len();
+        let transactions = entries.iter().map(|e| e.transactions.len()).sum::<usize>();
         for entry in entries {
             for transaction in entry.transactions {
                 if transaction.message.static_account_keys().contains(&PUMPFUN_MINT_AUTHORITY) {
                     let signature = transaction.signatures.first().unwrap();
-                    println!("[{}][{:?}]", slot_entry.slot, signature);
+                    println!("[{}, {}, {}][{:?}]", slot_entry.slot, entries_size, transactions, signature);
                 }
             }
         }
